9aa2553146b5f9aedcb072b4b0fba11b4a6c0796
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-05-23 01:39:29 -0300

    Inserts Produtos

---
+++ NoSQL-project-Pedro/insertsProdutos.js
@@ -0,0 +1,301 @@
+    // Coleção Produtos
+
+
+
+/* Inserts na Coleção Produtos */
+
+
+
+db.produtos.insert({
+
+	codigo : 1,
+
+	nome : 'Already Over',
+
+	preco : 3.85,
+
+	descricao : 'Musica',
+
+	artista : 'RED',
+
+	cliente : [{nome : 'Luana', sobrenome : 'Barbosa'}]
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 2,
+
+	nome : 'Phenomenom',
+
+	preco : 4.99,
+
+	descricao : 'Musica',
+
+	artista : 'Thousand Foot Krutch'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 3,
+
+	nome : 'One',
+
+	preco : 6.11,
+
+	descricao : 'Musica',
+
+	artista : 'U2',
+
+	cliente : [{nome : 'Roberto', sobrenome : 'Santiago'}]
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 4,
+
+	nome : 'Offering',
+
+	preco : 7.49,
+
+	descricao : 'Musica',
+
+	artista : 'Third Day'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 5,
+
+	nome : 'Only Hope',
+
+	preco : 2.64,
+
+	descricao : 'Musica',
+
+	artista : 'Switchfoot'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 6,
+
+	nome : 'Stay Close',
+
+	preco : 8.99,
+
+	descricao : 'Musica',
+
+	artista : 'Fireflight'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 7,
+
+	nome : 'Incondicional',
+
+	preco : 5.00,
+
+	descricao : 'Musica',
+
+	artista : 'Oficina G3'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 8,
+
+	nome : 'Everything',
+
+	preco : 4.99,
+
+	descricao : 'Musica',
+
+	artista : 'Lifehouse'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 21,
+
+	nome : 'Thrive',
+
+	preco : 18.00,
+
+	descricao : 'Album',
+
+	artista : 'Casting Crowns'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 22,
+
+	nome : 'Unspoken',
+
+	preco : 13.86,
+
+	descricao : 'Album',
+
+	artista : 'Unspoken'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 23,
+
+	nome : 'All of the Above',
+
+	preco : 16.90,
+
+	descricao : 'Album',
+
+	artista : 'Hillsong United'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 24,
+
+	nome : 'Rise',
+
+	preco : 14.10,
+
+	descricao : 'Album',
+
+	artista : 'SKillet',
+
+	cliente : [{nome : 'Maria', sobrenome : 'Luiza'}]
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 25,
+
+	nome : 'Humanos',
+
+	preco : 19.99,
+
+	descricao : 'Album',
+
+	artista : 'Oficina G3',
+
+	cliente : [{nome : 'Patricia', sobrenome : 'Ferreira'}]
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 26,
+
+	nome : 'Restart',
+
+	preco : 21.34,
+
+	descricao : 'Album',
+
+	artista : 'Newsboys'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 27,
+
+	nome : 'Jesus Freak',
+
+	preco : 19.70,
+
+	descricao : 'Album',
+
+	artista : 'DC Talk'
+
+
+
+});
+
+
+
+db.produtos.insert({
+
+	codigo : 28,
+
+	nome : 'Testify',
+
+	preco : 15.22,
+
+	descricao : 'Album',
+
+	artista : 'P.O.D.'
+
+
+
+});
