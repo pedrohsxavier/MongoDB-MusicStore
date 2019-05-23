04545e376fc4dd1a061bbcbb92a5109fb742a965
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-05-23 01:36:38 -0300

    Inserts Artistas

---
+++ NoSQL-project-Pedro/insertsArtistas.js
@@ -0,0 +1,313 @@
+    // Coleção Artistas
+
+
+
+/* Inserts na Coleção Artistas */
+
+
+
+db.artistas.insert({
+
+	nome : 'RED',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'Nashville',
+
+	anoFundacao : '2004',
+
+	genero : 'Rock',
+
+	cliente : [{nome : 'Luana', sobrenome : 'Barbosa'}],
+
+	produto : [{codigo : 1, nome : 'Already Over', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Casting Crowns',
+
+	pais : 'Estados Unidos',
+
+	anoFundacao : '1999',
+
+	genero : 'Rock',
+
+	produto : [{codigo : 21, nome : 'Thrive', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Lifehouse',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'Malibu',
+
+	anoFundacao : '1999',
+
+	genero : 'Rock',
+
+	produto : [{codigo : 8, nome : 'Everything', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Hillsong United',
+
+	pais : 'Australia',
+
+	anoFundacao : '1999',
+
+	genero : 'Pop Rock',
+
+	cliente : [{nome : 'Luana', sobrenome : 'Barbosa'}],
+
+	produto : [{codigo : 23, nome : 'All of the Above', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Unspoken',
+
+	pais : 'Estados Unidos',
+
+	anoFundacao : '2003',
+
+	genero : 'Pop Rock',
+
+	produto : [{codigo : 22, nome : 'Unspoken', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'U2',
+
+	pais : 'Irlanda',
+
+	anoFundacao : '1976',
+
+	genero : 'Rock',
+
+	cliente : [{nome : 'Roberto', sobrenome : 'Santiago'}],
+
+	produto : [{codigo : 3, nome : 'One', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Skillet',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'Memphis',
+
+	anoFundacao : '1996',
+
+	genero : 'Rock',
+
+	cliente : [{nome : 'Maria', sobrenome : 'Luiza'}],
+
+	produto : [{codigo : 24, nome : 'Rise', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'P.O.D.',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'San Diego',
+
+	anoFundacao : '1992',
+
+	genero : 'Metal',
+
+	cliente : [{nome : 'Maria', sobrenome : 'Luiza'}],
+
+	produto : [{codigo : 28, nome : 'Testify', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Switchfoot',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'San Diego',
+
+	anoFundacao : '1997',
+
+	genero : 'Rock',
+
+	produto : [{codigo : 5, nome : 'Only Hope', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Third Day',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'Marietta',
+
+	anoFundacao : '1995',
+
+	genero : 'Rock',
+
+	cliente : [{nome : 'Patricia', sobrenome : 'Ferreira'}],
+
+	produto : [{codigo : 4, nome : 'Offering', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Thousand Foot Krutch',
+
+	pais : 'Canada',
+
+	cidade : 'Ontario',
+
+	anoFundacao : '1997',
+
+	genero : 'Hard Rock',
+
+	cliente : [{nome : 'Roberto', sobrenome : 'Santiago'}],
+
+	produto : [{codigo : 2, nome : 'Phenomenom', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'DC Talk',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'Lynchburg',
+
+	anoFundacao : '1989',
+
+	genero : 'Rock',
+
+	produto : [{codigo : 27, nome : 'Jesus Freak', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Fireflight',
+
+	pais : 'Estados Unidos',
+
+	cidade : 'Eustis',
+
+	anoFundacao : '1999',
+
+	genero : 'Rock',
+
+	produto : [{codigo : 6, nome : 'Stay Close', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Oficina G3',
+
+	pais : 'Brasil',
+
+	cidade : 'Sao Paulo',
+
+	anoFundacao : '1987',
+
+	genero : 'Hard Rock',
+
+	cliente : [{nome : 'Patricia', sobrenome : 'Ferreira'}],
+
+	produto : [{codigo : 25, nome : 'Humanos', descricao : 'Album'}, {codigo : 7, nome : 'Incondicional', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.artistas.insert({
+
+	nome : 'Newsboys',
+
+	pais : 'Australia',
+
+	cidade : 'Mooloolaba',
+
+	anoFundacao : '1985',
+
+	genero : 'Pop',
+
+	produto : [{codigo : 26, nome : 'Restart', descricao : 'Album'}]
+
+
+
+});
