b1b3d8dbbffe027179ee57531c70609f95fedb52
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-05-23 01:37:58 -0300

    Inserts Clientes

---
+++ NoSQL-project-Pedro/insertsClientes.js
@@ -0,0 +1,401 @@
+    // Coleção Clientes
+
+
+
+/* Inserts na Coleção Clientes */
+
+
+
+db.clientes.insert({
+
+	nome : 'Luana',
+
+	sobrenome : 'Barbosa',
+
+	login : 'lubarbosa',
+
+	senha : '051268',
+
+	plano : 'Free',
+
+	email : ['lubarbosa@gmail.com', 'barbosalu@gmail.com'],
+
+	telefone : ['98865-7260', '98174-9090'],
+
+	endereco : {cidade : 'Campina Grande',
+
+		    bairro : 'Centro',
+
+		    rua : 'Adenor Tavares'
+
+		   },
+
+	artista : [{
+
+		  nome : 'Hillsong United',
+
+		  genero : 'Pop Rock'
+
+		  },
+
+		  {
+
+		  nome : 'RED',
+
+		  genero : 'Rock'
+
+		  }],
+
+	produto : [{codigo : 1, nome : 'Already Over', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Roberto',
+
+	sobrenome : 'Santiago',
+
+	login : 'robinho',
+
+	senha : '42am77',
+
+	plano : 'Gold',
+
+	email : ['santiagorobinho@gmail.com'],
+
+	telefone : ['98780-1245', '99860-3398'],
+
+	endereco : {cidade : 'Recife',
+
+		    bairro : 'Prado',
+
+		    rua : 'Teodoro Lima Abrantes'
+
+		   },
+
+	artista : [{
+
+		  nome : 'Thousand Foot Krutch',
+
+		  genero : 'Hard Rock'
+
+		  },
+
+		  {
+
+		  nome : 'U2',
+
+		  genero : 'Rock'
+
+		  }],
+
+	produto : [{codigo : 3, nome : 'One', descricao : 'Musica'}]
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Maria',
+
+	sobrenome : 'Luiza',
+
+	login : 'malu',
+
+	senha : 'lu152008',
+
+	plano : 'Premium',
+
+	email : ['malu20@gmail.com', 'luizam@hotmail.com'],
+
+	telefone : ['98710-6744'],
+
+	endereco : {cidade : 'Salvador',
+
+		    bairro : 'Centro',
+
+		    rua : 'Galdino Ferreira'
+
+		   },
+
+	artista : [{
+
+		  nome : 'Skillet',
+
+		  genero : 'Rock'
+
+		  },
+
+		  {
+
+		  nome : 'P.O.D.',
+
+		  genero : 'Metal'
+
+		  }],
+
+	produto : [{codigo : 24, nome : 'Rise', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Patricia',
+
+	sobrenome : 'Ferreira',
+
+	login : 'patyfe',
+
+	senha : 'fe806530',
+
+	plano : 'Free',
+
+	telefone : ['98870-9635'],
+
+	endereco : {cidade : 'Rio de Janeiro',
+
+		    bairro : 'Taquara',
+
+		    rua : 'Quirino Fagundes'
+
+		   },
+
+	artista : [{
+
+		  nome : 'Third Day',
+
+		  genero : 'Rock'
+
+		  },
+
+		  {
+
+		  nome : 'Oficina G3',
+
+		  genero : 'Hard Rock'
+
+		  }],
+
+	produto : [{codigo : 25, nome : 'Humanos', descricao : 'Album'}]
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Mario',
+
+	sobrenome : 'Ricardo',
+
+	login : 'ricmar',
+
+	senha : 'marioricardo7070',
+
+	plano : 'Gold',
+
+	email : ['mario_ricardo@gmail.com', 'marioric@hotmail.com'],
+
+	telefone : ['99971-5200', '98655-9276'],
+
+	endereco : {cidade : 'Rio de Janeiro',
+
+		    bairro : 'Campo Grande',
+
+		    rua : 'Maximiliano Machado'
+
+		   }
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Katia',
+
+	sobrenome : 'Meireles',
+
+	login : 'kameireles',
+
+	senha : 'kameireles8513',
+
+	plano : 'Premium',
+
+	email : ['ka_meireles@gmail.com'],
+
+	endereco : {cidade : 'Porto Alegre',
+
+		    bairro : 'Centro',
+
+		    rua : 'Justino Silveira'
+
+		   }
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Ronaldo',
+
+	sobrenome : 'Medeiros',
+
+	login : 'ronaldo10',
+
+	senha : 'ronny10',
+
+	plano : 'Gold',
+
+	email : ['ronnymedeiros@outlook.com', 'ronaldo_med10@gmail.com'],
+
+	telefone : ['98716-3038', '98679-0090'],
+
+	endereco : {cidade : 'Santos',
+
+		    bairro : 'Vila Belmiro',
+
+		    rua : 'Valenciano Veloso'
+
+		   }
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Carla',
+
+	sobrenome : 'Martins',
+
+	login : 'martinscarlinha',
+
+	senha : 'carlinha102030',
+
+	plano : 'Premium',
+
+	email : ['carlinha_martins@gmail.com'],
+
+	telefone : ['98850-9480', '98762-9941'],
+
+	endereco : {cidade : 'Natal',
+
+		    bairro : 'Potengi',
+
+		    rua : 'Caldeira das Luzes'
+
+		   }
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Paulo',
+
+	sobrenome : 'Emanuel',
+
+	login : 'emanuelpaulo',
+
+	senha : 'manuel2341',
+
+	plano : 'Gold',
+
+	email : ['paulo_emanuel@hotmail.com', 'emanuelpaulo@gmail.com'],
+
+	telefone : ['99684-0510', '98885-4066'],
+
+	endereco : {cidade : 'Belo Horizonte',
+
+		    bairro : 'Centro',
+
+		    rua : 'Joaquim Santos Borges'
+
+		   }
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Marta',
+
+	sobrenome : 'Barros',
+
+	login : 'martinhabarros',
+
+	senha : 'bmartinha18740',
+
+	plano : 'Free',
+
+	email : ['martinha_barros@gmail.com', 'barrosmar@hotmail.com'],
+
+	telefone : ['98790-5548', '99892-9736'],
+
+	endereco : {cidade : 'Rio de Janeiro',
+
+		    bairro : 'Barra da Tijuca',
+
+		    rua : 'Horcades Santos de Souza'
+
+		   }
+
+
+
+});
+
+
+
+db.clientes.insert({
+
+	nome : 'Lauro',
+
+	sobrenome : 'Fernandes',
+
+	login : 'laurofe',
+
+	senha : 'lauro1221',
+
+	plano : 'Gold',
+
+	email : ['lauro_fernandes@gmail.com', 'laurofernandes@hotmail.com'],
+
+	endereco : {cidade : 'Belo Horizonte',
+
+		    bairro : 'Centro',
+
+		    rua : 'Roberto Lima Penantes'
+
+		   }
+
+
+
+});
